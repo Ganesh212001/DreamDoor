22e29fcd11fa2351c9bf323568e8885b43c79ae2
Ganesh <ganeshmahajan21062001@gmail.com>
2023-05-15 16:43:38 +0530

    add node start

--- backend/server.js
+++ backend/server.js
@@ -84,11 +84,11 @@ app.listen(PORT, '0.0.0.0', () => { console.log(`Server running on port ${PORT}`
 // Place after routes
 // Render => Serving the frontend
 
-app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
+// app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+// });
 
 // app.use(notFound)
 // app.use(errorHandler)
